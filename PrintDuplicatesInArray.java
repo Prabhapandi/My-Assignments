717ba4ef9423d5fe5083f542a5a5d6daef81b68d
Prabhapandi <126978751+Prabhapandi@users.noreply.github.com>
2023-03-08 21:30:16 +0530

    Add files via upload
    Week1.day2

---
+++ PrintDuplicatesInArray.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class PrintDuplicatesInArray {
+
+	public static void main(String[] args) {
+		int[] arr = {14,12,13,11,15,14,18,16,17,19,18,17,20};
+		
+		int len =arr.length;
+		
+			for (int i = 0; i < len; i++) {
+				
+					for (int j = i+1; j < len-1; j++) {
+						
+						if (arr[i]==arr[j]) {
+							System.out.println("dupicate no is:"+arr[i]);
+						
+							
+						} 
+					}	
+					}
+	}
+}
+					
+			
+

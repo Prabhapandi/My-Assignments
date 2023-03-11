15d27ecc24d5013c98da1b7eace79088ac09b0fd
Prabhapandi <126978751+Prabhapandi@users.noreply.github.com>
2023-03-11 22:20:11 +0530

    Add files via upload
    Week2day1

---
+++ Assignment1.java
@@ -0,0 +1,58 @@
+package week2day1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ById;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		EdgeDriver driver = new EdgeDriver();
+		driver.get("http://leaftaps.com/opentaps");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoCSR");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.findElement(By.partialLinkText("CRM")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("HCL");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Prabha");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Pandi");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Software Testing");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("Do Regression -- high level Sanity");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("prabha.baab@gmail.com");
+		WebElement state = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+        Select sel = new Select(state);
+        sel.selectByVisibleText("New York");
+        driver.findElement(By.name("submitButton")).click();
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_description")).clear();
+		driver.findElement(By.id("updateLeadForm_importantNote")).sendKeys("Check for new codes");
+		driver.findElement(By.name("submitButton")).click();
+		String title1 = driver.getTitle();
+		System.out.println(title1);
+
+
+
+
+
+		
+		
+		
+		
+		
+		
+
+	
+
+	}
+
+
+
+
+	}
+
+
---
+++ Assignment3.java
@@ -0,0 +1,47 @@
+package week2day1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ById;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		EdgeDriver driver = new EdgeDriver();
+		driver.get("http://leaftaps.com/opentaps");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoCSR");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.findElement(By.partialLinkText("CRM")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("HCL");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Prabha");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Pandi");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Software Testing");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("Do Regression -- high level Sanity");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("prabha.baab@gmail.com");
+		WebElement state = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+        Select sel = new Select(state);
+        sel.selectByVisibleText("New York");
+        driver.findElement(By.name("submitButton")).click();
+        String title2 = driver.getTitle();
+		System.out.println(title2);
+		driver.findElement(By.linkText("Duplicate Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).clear();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("HCL TECH");
+		driver.findElement(By.id("createLeadForm_firstName")).clear();
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("vasantha");
+        driver.findElement(By.name("submitButton")).click();
+        String title3 = driver.getTitle();
+        System.out.println(title3);
+
+
+
+		
+	}
+
+}
